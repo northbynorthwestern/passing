2c583f86d716fef51ba1c26f74dce58752025c0a
duner <asduner@gmail.com>
2015-02-22 17:53:57 -0600

    yay css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function() {
+
+  var active;
+  var slides = $('.slide');
+
+
+  $('.begin').click(function() {
+    setActiveSlide('next');
+  });
+
+  function setActiveSlide(direction) {
+
+    if (direction === 'previous') {
+      active = $('.active').prev();
+    } else {
+      active = $('.active').next();
+    }
+
+    for (var i = slides.length - 1; i >= 0; i--) {
+      $(slides[i]).removeClass('active');
+    }
+
+    $(active).addClass('active');
+
+    return active;
+  }
+
+});
