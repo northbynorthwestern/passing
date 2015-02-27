7a50f044b03e7385c41850e81419e93c0e6d1eaa
duner <asduner@gmail.com>
2015-02-26 20:13:49 -0600

    Button CSS and JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
 
   var active;
   var slides = $('.slide');
-
+  $('.arrows').hide();
+  $('.audio-player').hide();
 
   $('.begin').click(function() {
-    setActiveSlide('next');
+    active = setActiveSlide('next');
   });
 
   function setActiveSlide(direction) {
-
     if (direction === 'previous') {
       active = $('.active').prev();
     } else {
@@ -24,7 +24,70 @@ $(document).ready(function() {
 
     $(active).addClass('active');
 
+    updateAudioPlayer();
+    updateArrows();
+    playActiveAudio();
+
     return active;
   }
 
+  $('.arrow.previous').click(function(){ active = setActiveSlide('previous'); });
+  $('.arrow.next').click(function(){ active = setActiveSlide(); });
+
+
+  function updateAudioPlayer() {
+    if ($(active).hasClass('title')) {
+      $('.audio-player').hide();
+    } else {
+      $('.audio-player').show();
+    }
+
+  }
+
+  function updateArrows() {
+    if ($(active).prev().hasClass('slide')) {
+      $('.arrow.previous').show();
+    } else {
+      $('.arrow.previous').hide();
+    }
+
+    if ($(active).next().hasClass('slide')) {
+    } else {
+      $('.arrow.next').hide();
+    }
+
+    if ($(active).hasClass('title')) {
+      $('.arrows').hide();
+    } else {
+      $('.arrows').show();
+    }
+  }
+
+  function onAudioEnded() {
+    deBlurActive();
+  }
+
+  var $audioPlayer = $('#jp_container');
+  $audioPlayer.jPlayer({
+    ended: onAudioEnded,
+    supplied: 'mp3',
+    loop: false,
+    timeupdate: onTimeUpdate,
+    swfPath: '/bower_components/jplayer/dist/jplayer/jquery.jplayer.swf'
+  });
+
+  function playActiveAudio() {
+    var audioUrl = $(active).data('audiourl');
+    if (audioUrl) {
+      $audioPlayer.jPlayer('setMedia', {
+        mp3: audioUrl
+      }).jPlayer('play');
+    }
+  }
+
+  function deBlurActive() {
+    $('.active > .bg-img').toggleClass('blur');
+  }
+
+  function onTimeUpdate() { }
 });
